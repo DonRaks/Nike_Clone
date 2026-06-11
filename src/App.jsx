606acfc3528ba15018efe819d5bb6cefb1476a3b
DonRaks <orakadonald@gmail.com>
2026-06-11 18:08:27 +0100

    created the structure of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+
+import { Hero, PopularProducts, SuperQuality, Services,SpecialOffer, CustomerReviews, Subscribe,Footer} from './sections';
 
 const App = () => {
+  
   return (
-    <div className='text-3xl font-bold underline'>
-      app
-    </div>
-  )
-}
+    <main className="relative">
+      <div>Nav</div>
 
-export default App
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+
+      <section className="padding">
+        <PopularProducts />   
+      </section>
+
+      <section className="padding">
+        <SuperQuality />
+      </section>
+
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+
+      <section className='bg-sky-50 padding' >
+        <CustomerReviews />
+      </section>
+
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+
+      <section className=' bg-black padding-x padding-t pb-8' >
+        <Footer />
+      </section>
+    </main> 
+  );
+};
+
+export default App;
